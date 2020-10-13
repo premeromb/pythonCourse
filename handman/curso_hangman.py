af2c61667be4d6544dc50d28a80e1eb8a66f53e2
promeromb <promeromb@alumnos.unex.es>
2020-10-13 16:10:10 +0200

    Stage 8

--- handman/curso_hangman.py
+++ handman/curso_hangman.py
@@ -4,53 +4,60 @@ print("H A N G M A N ")
 
 words = 'python', 'java', 'kotlin', 'javascript'
 
-word = random.choice(words)
-
-wordSet = set(word)
-
 gessedSet = set()  
 
 failedSet = set()
 
-lives = 8
-win = False
+while(True):
+    choice = input('Type "play" to play the game, "exit" to quit: ')
+    if !(choice == "play") and choice == "exit":
+        exit()
 
-while lives != 0:
+    word = random.choice(words)
 
-    print("")
+    wordSet = set(word)
 
-    #print current caracters
-    for i in range(len(word)):
-        if word[i] in gessedSet:
-            print(word[i], end="")
+    gessedSet.clear()
+
+    failedSet.clear
+
+    lives = 8
+
+    while lives != 0:
+        print("")
+
+        #print current caracters
+        for i in range(len(word)):
+            if word[i] in gessedSet:
+                print(word[i], end="")
+            else:
+                print("-", end="")
+
+        print("")
+
+        if set(wordSet) == set(gessedSet):
+            print("You guessed the word!")
+            print("You survived!")
+            break
+
+        read = input("Input a letter: ")
+
+        if 0 < len(read) > 1:
+            print("You should input a single letter")
+            continue
+        if read.islower() == False:
+            print("Please enter a lowercase English letter")
+            continue
+        if read in gessedSet or read in failedSet:
+            print("You've already guessed this letter")
+            continue
+        elif read in word:
+            gessedSet.add(read)
         else:
-            print("-", end="")
+            print("That letter doesn't appear in the word")
+            lives -= 1
+            failedSet.add(read)
 
-    print("")
-
-    if set(wordSet) == set(gessedSet):
-        print("You guessed the word!")
-        print("You survived!")
-        break
-
-    read = input("Input a letter: ")
-
-    if 0 < len(read) > 1:
-        print("You should input a single letter")
-        continue
-    if read.islower() == False:
-        print("Please enter a lowercase English letter")
-        continue
-    if read in gessedSet or read in failedSet:
-        print("You've already guessed this letter")
-        continue
-    elif read in word:
-        gessedSet.add(read)
     else:
-        print("That letter doesn't appear in the word")
-        lives -= 1
-        failedSet.add(read)
-
-else:
-    print("You lost!")
+        print("You lost!")
 
