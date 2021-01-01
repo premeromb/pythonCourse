bcbefd6d2f323c8603a6959587f941a6b269e43f
promeromb <promeromb@alumnos.unex.es>
2021-01-01 22:56:13 +0100

    Stage 3

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -3,10 +3,7 @@ from random import randrange
 DIMENSION = 3
 grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
-user_player = 'X'
-computer_player = 'O'
-
-game_level = 'easy'
+game_turn = 'X'
 
 scheme = [  [[0,0], [0,1], [0,2]],  # rows
             [[1,0], [1,1], [1,2]], 
@@ -56,6 +53,13 @@ def change_turn():
     else:
         game_turn = 'X'
 
+def initialize_game():
+    draw_cells()
+
+def clear_grid():
+    grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
+
 def is_occupied(coordinates):
     if grid[coordinates[0] - 1][coordinates[1] - 1] == ' ':
         return False
@@ -82,14 +86,14 @@ def is_valid_coordinates(coordinates):
         print("This cell is occupied! Choose another one!")
         return False
     else:
-        return True
-        
+        return True  
 
-def add_to_grid(coordinates, game_turn):
+def add_to_grid(coordinates):
     grid[coordinates[0] - 1][coordinates[1] - 1] = game_turn
     draw_cells()
+    change_turn()
 
-def read_next_move(game_turn):
+def read_next_move():
     done = False
     while not done:
         try:
@@ -100,9 +104,7 @@ def read_next_move(game_turn):
         except ValueError:
             print("You should enter numbers!")
         
-    add_to_grid(coordinates, game_turn)
-
-
+    add_to_grid(coordinates)
 
 def check_initial_turn():
     if num_X > num_O:
@@ -125,42 +127,95 @@ def is_grid_complete():
                 return False
     return True
 
-def check_game_state():
+def game_ended():
     if is_player_win('X'):
         print("X wins")
-        exit()
     elif is_player_win('O'):
         print("O wins")
-        exit()
     elif is_grid_complete():
         print("Draw")
-        exit()
 
-def initialize_game():
-    draw_cells()
+def is_end_game():
+    if is_player_win('X') or is_player_win('O') or is_grid_complete():
+        return True
+    else:
+        return False
 
 def user_next_move():
-    read_next_move(user_player)
-    check_game_state()
+    read_next_move()
 
-def computer_next_move():
-
+def computer_next_move(game_level):
     if game_level == 'easy':
         print('Making move level "easy"')
         coordinates = [randrange(3), randrange(3)]
         while is_occupied(coordinates):
             coordinates = [randrange(3), randrange(3)]
-        add_to_grid(coordinates, computer_player)
-
-    check_game_state()
+        add_to_grid(coordinates)
 
 
-initialize_game()
+def game_user_user():
+    while not is_end_game():
+        user_next_move()
+        if not is_end_game():
+            user_next_move()
 
-while(True):
-    user_next_move()
+def game_user_computer(game_level):
+    while not is_end_game():
+        user_next_move()
+        if not is_end_game():
+            computer_next_move(game_level)
+
+def game_computer_user(game_level):
+    while not is_end_game():
+        ucomputer_next_move(game_level)
+        if not is_end_game():
+            user_next_move()
+
+def game_computer_computer(game_level_1, game_level_2):
+    while not is_end_game():
+        computer_next_move(game_level_1)
+        if not is_end_game():
+            computer_next_move(game_level_2)
+
+
+def is_correct_parameters(parameters):
+    if len(parameters) not in [1, 3]:
+        print("Bad parameters!")
+        return False
+    elif len(parameters) == 1 and parameters[0] != 'exit':
+        print("Bad parameters!")
+        return False
+    elif len(parameters) == 3 and (parameters[1] not in ['user', 'easy'] or parameters[2] not in ['user', 'easy']):
+        print("Bad parameters!")
+        return False
+    else: 
+        return True
+
+def start_menu(parameters):
+    if parameters[1] == 'user' and parameters[2] == 'user':
+        game_user_user()
+    elif parameters[1] == 'user' and parameters[2] == 'easy':
+        game_user_computer(parameters[2])
+    elif parameters[1] == 'easy' and parameters[2] == 'user':
+        game_computer_user(parameters[1])
+    elif parameters[1] == 'easy' and parameters[2] == 'easy':
+        game_computer_computer(parameters[1], parameters[2])
+
+    game_ended()
+    clear_grid()
+
+def menu():
+    parameters = input("Input command: ").split()
+    while not is_correct_parameters(parameters):
+        parameters = input("Input command: ").split()
     
-    computer_next_move()
+    if parameters[0] == 'exit':
+        exit()
+    elif parameters[0] == 'start':
+        initialize_game()
+        start_menu(parameters)
 
-
+while True:
+    menu()
+    print()
 
