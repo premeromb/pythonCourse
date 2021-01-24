94423c2c4874b5186d39989adb40a81e762bd82a
promeromb <promeromb@alumnos.unex.es>
2021-01-24 18:59:23 +0100

    Stage 4, multi language

--- online_translator/translator.py
+++ online_translator/translator.py
@@ -2,72 +2,90 @@ import requests
 
 from bs4 import BeautifulSoup
 
-
-languages = {"fr": "french", "en": "english"}
-
+languages = {
+    '1': 'arabic',
+    '2': 'german',
+    '3': 'english',
+    '4': 'spanish',
+    '5': 'french',
+    '6': 'hebrew',
+    '7': 'japanese',
+    '8': 'dutch',
+    '9': 'polish',
+    '10': 'portuguese',
+    '11': 'romanian',
+    '12': 'russian',
+    '13': 'turkish'
+}
 language_from = ""
 language_to = ""
 word = ""
+url = " "
 
-def make_request(url):
 
-    user_agent = 'Mozilla/5.0'
-    page = requests.get(url, headers={'User-Agent': user_agent})
+def print_data_request(soup):
 
-    if page.status_code == 200:
-        print("200 OK")
-
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    translation = soup.find_all('a', class_='translation')
-
-    tr = []
-    for result in translation:
-        tr.extend(result.text.split())
-    tr.pop()
-    tr.pop(0)
-
-    print("\nContext examples: \n")
-    print(languages[language_to].capitalize() + " Translations:")
-
-    for index in range(5):
-        print(tr[index])
+    # TRANSLATIONS
+    print("\n" + languages[language_to].capitalize() + " Translations:")
     
-    examples = soup.find_all('div', class_='src ltr')
+    translation = soup.find_all('a', class_='translation')
+    translation.pop(0)
 
-    examples_translated = soup.find_all('div', class_='trg ltr')
+    examples = soup.find_all('a', {"class": "translation"})
+    examples.pop(0)
+    examples.pop()
 
+    for iterable in range(5):
+        if len(examples) > iterable:
+                print(examples[iterable].text.strip())
+
+    # EXAMPLES
+    examples_translated = soup.find_all('div', {"class": "ltr"})
+
+    counter = 0
 
     print("\n" + languages[language_to].capitalize() + " Examples:")
 
-    for iterable in range(5):
-        print(examples[iterable].text.strip(), end=':\n')
-        print(examples_translated[iterable].text.strip(), end='\n\n')
+    sentence_list = [x.text.strip() for x in soup.select("#examples-content .text")]
+
+    for i in range(5):
+        print(f"{sentence_list[i * 2]}\n{sentence_list[i * 2 + 1]}\n")
+
+def code_check(status_code):
+    print(f"{status_code} OK") if status_code == 200 else print(f"{status_code} NOT OK")
+
+def web_service_connect(url):
+    user_agent = 'Mozilla/5.0'
+    page = requests.get(url, headers={'User-Agent': user_agent})
+    code_check(page.status_code)
+    return BeautifulSoup(page.content, 'html.parser')
 
 
-def translation():
+def set_url():
+    global url
     url = 'https://context.reverso.net/translation/' + languages[language_from] + '-' + languages[language_to] + '/' + word 
-    make_request(url)
 
 
 def select_language():
-    global language_from, language_to, word
-    print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+    global language_to, language_from
+    print("Hello, you're welcome to the translator. Translator supports: ")
+
+    for  key, value in languages.items():
+        print(key + ". " + value.capitalize())
+
+    print('Type the number of your language:')
+    language_from = input()
+    print('Type the number of language you want to translate to: ')
     language_to = input()
+
+def select_word():
+    global word
     print("Type the word you want to translate:")
-    word = input()
-
-    if language_to == "fr":
-        language_from = "en"
-    else:
-        language_from = "fr"
-
-    print('You chose "{}" as the leguage to translate "{}" to.'.format(language_to,
-                                                                       word))
-
+    word = input().lower()
 
 select_language()
+select_word()
 
-translation()
+set_url()
 
-# make_request('https://context.reverso.net/translation/english-french/cheese')
+print_data_request(web_service_connect(url))
